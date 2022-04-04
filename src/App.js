c15644f3861bd274c3d2b688d2880737073eba51
proyceus <proyceus@gmail.com>
2022-04-04 16:01:30 -0400

    Add item functionality finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import {useState} from 'react';
+import AddInventoryPage from './Components/AddInventoryPage/AddInventoryPage';
+
 
 function App() {
+
+  const [itemList, setItemList] = useState([]);
+  const [item, setItem] = useState('');
+  const [box, setBox] = useState('');
+
+
+  const handleAddItem = (e) => {
+    e.preventDefault();
+
+    const itemInfo = {
+      name: item,
+      boxNumber: box
+    }
+
+    setItemList((prevState) => 
+      [...prevState, itemInfo]
+    )
+  }
+
   return (
     <div className="App">
-      <p>This is my app!</p>
+      <p>Testing</p>
+      <AddInventoryPage 
+        setItem={setItem}
+        setBox={setBox}
+        items={itemList}
+        handleAddItem={handleAddItem}
+       />
     </div>
   );
 }
---
+++ src/Components/AddInventoryPage/AddInventoryPage.js
@@ -0,0 +1,22 @@
+const AddInventoryPage = ({setItem, setBox, items, handleAddItem}) => {
+
+
+    return (
+        <div>
+          <div className="addinventory">
+            <input type="text" placeholder="Add an item..." onChange={(e) => setItem(e.target.value)} />
+            <input type="text" placeholder="Box number..." onChange={(e) => setBox(e.target.value)} />
+            <button onClick={handleAddItem}>Submit</button>
+          </div>
+          <div className="itemlist">
+              <ul>
+                  {items && items
+                    .map((item) => <li>Name: {item.name} Box: {item.boxNumber}</li>)}
+              </ul>
+          </div>
+        </div>
+        
+    )
+}
+
+export default AddInventoryPage;
