fa7bf1f5daf1d45e54b78f21b0b1d2d96f86fad0
proyceus <proyceus@gmail.com>
2022-04-21 17:23:11 -0400

    updated server code for routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,50 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const passportLocal = require('passport-local').Strategy;
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
+const User = require('./user');
+
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+const uri = process.env.MONGO_URI;
+
+// Connect and configure to MongoDB Database
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error(err));
+
+
+// Connect and configure Express
 const app = express();
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded());
 
-app.use('/login', (req, res) => {
-    res.send(req.body)
+app.use(session({
+  secret: "secretcode",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./passportConfig')(passport);
+
+// Import Express routes
+require('./routes')(app);
+
+
+// Start Express Server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
 });
-
-
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
