043678f5d5aeb04018dbec203500b74fee2c2a9b
proyceus <proyceus@gmail.com>
2022-04-21 17:15:48 -0400

    fixed the render file to conform to new rules

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter} from 'react-router-dom';
+import ReactDOM from "react-dom/client";
 
 
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+<BrowserRouter>
+  <App />
+</BrowserRouter>
+)
