aac287d1396ea2c13de10c3a89a56a613f8dc7e2
proyceus <proyceus@gmail.com>
2022-04-26 15:57:02 -0400

    added signup route for mongodb

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,13 +1,37 @@
 const passport = require("passport");
-const User = require("./user");
+const User = require("../user");
 const bcrypt = require("bcryptjs");
+const { getToken, COOKIE_OPTIONS, getRefreshToken } = require("../authenticate");
 
 module.exports = function (app) {
-  app.use("/login", (req, res) => {
-    res.send(req.body);
+  
+  app.post("/signup", (req, res, next) => {
+    User.register(
+      new User({ username: req.body.username }),
+      req.body.password,
+      (err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.send(err);
+        } else {
+          const token = getToken({_id: user._id});
+          const refreshToken = getRefreshToken({_id: user._id});
+          user.refreshToken.push({refreshToken});
+          user.save((err, user) => {
+            if (err) {
+              res.statusCode = 500;
+              res.send(err)
+            } else {
+              res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS);
+              res.send({success: true, token})
+            }
+          })
+        }
+      }
+    )
   });
 
-  app.use("/", (req, res) => {
-    res.send("Server beep boop")
-  })
+
+
+  
 };
